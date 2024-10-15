298b80b32aae2d21d05c153d10aacf445685af35
Rashedul-Alam-Rudra <rashedulalam.iit.du@gmail.com>
2024-10-15 18:39:09 +0300

    added npg sql for connection strings

---
+++ EmployeeManagementService/src/EmployeeManagementService.Infrastructure.Persistence/DependencyRegistration.cs
@@ -0,0 +1,27 @@
+﻿using EmployeeManagementService.Infrastructure.persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeManagementService.Infrastructure.Persistence
+{
+    public static class DependencyRegistration
+    {
+        public static IServiceCollection RegisterPersistenceDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContextPool<EmployeeDbContext>(builder =>
+            {
+                builder.UseNpgsql(configuration.GetConnectionString("default"), optionsBuilder =>
+                    {
+                        optionsBuilder.MigrationsAssembly(typeof(EmployeeDbContext).Assembly.FullName);
+                        optionsBuilder.CommandTimeout(300);
+                    })
+                    .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
--- EmployeeManagementService/src/EmployeeManagementService.Infrastructure.Persistence/UnitOfWork.cs
+++ EmployeeManagementService/src/EmployeeManagementService.Infrastructure.Persistence/UnitOfWork.cs
@@ -4,11 +4,20 @@ using EmployeeManagementService.Infrastructure.Persistence.Repositories;
 
 namespace EmployeeManagementService.Infrastructure.Persistence;
 
-public class UnitOfWork(EmployeeDbContext employeeDbContext) : IUnitOfWork
+public class UnitOfWork(EmployeeDbContext employeeDbContext) : IUnitOfWork, IDisposable, IAsyncDisposable
 {
     public IGenericRepository<Employee, int> EmployeeRepository => new GenericRepository<Employee, int>(employeeDbContext);
 
     public IGenericRepository<Department, string> DepartmentRepository =>
         new GenericRepository<Department, string>(employeeDbContext);
 
+    public void Dispose()
+    {
+        employeeDbContext?.Dispose();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (employeeDbContext != null) await employeeDbContext.DisposeAsync();
+    }
 }
