fc5c118decabb7ac4dc7e450e8733f613a03805a
Rashedul-Alam-Rudra <rashedulalam.iit.du@gmail.com>
2024-10-15 14:10:19 +0300

    initialized the backend service

---
+++ EmployeeManagementService/src/EmployeeManagementService.Domain/Employee/Department.cs
@@ -0,0 +1,11 @@
+﻿using EmployeeManagementService.Domain.Common;
+
+namespace EmployeeManagementService.Domain.Employee
+{
+    public class Department : AuditableBaseEntity<string>
+    {
+        public string Name { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeManagementService/src/EmployeeManagementService.Domain/Employee/Employee.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagementService.Domain.Common;
+
+namespace EmployeeManagementService.Domain.Employee
+{
+    public class Employee : AuditableBaseEntity<int>
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string HireDate { get; set; }
+
+        public string DepartmentId { get; set; }
+
+        public Department Department { get; set; }
+    }
+}
