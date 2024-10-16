f24a1b1d14f6720a622037107a76260505aab0b1
Rashedul-Alam-Rudra <rashedulalam.iit.du@gmail.com>
2024-10-16 20:13:06 +0300

    added migrations

--- EmployeeManagementService/src/EmployeeManagementService/Infrastructure/ExceptionHandlers/GlobalExceptionHandler.cs
+++ EmployeeManagementService/src/EmployeeManagementService/Infrastructure/ExceptionHandlers/GlobalExceptionHandler.cs
@@ -4,20 +4,33 @@ using System.Net;
 
 namespace EmployeeManagementService.Infrastructure.ExceptionHandlers
 {
-    public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
+    public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger, IWebHostEnvironment env) : IExceptionHandler
     {
         public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
         {
             switch (exception)
             {
                 case DuplicateEntityException duplicateEntityException:
-                    await httpContext.Response.WriteAsJsonAsync(new { Errors = exception.Message }, cancellationToken: cancellationToken);
+                    await httpContext.Response.WriteAsJsonAsync(new { Errors = duplicateEntityException.Message }, cancellationToken: cancellationToken);
                     httpContext.Response.StatusCode = (int)HttpStatusCode.UnprocessableEntity;
                     break;
                 
                 case EntityNotFoundException entityNotFoundException:
-                    await httpContext.Response.WriteAsJsonAsync(new { Errors = exception.Message }, cancellationToken: cancellationToken);
+                    await httpContext.Response.WriteAsJsonAsync(new { Errors = entityNotFoundException.Message }, cancellationToken: cancellationToken);
                     httpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                    break;
+
+                default:
+                    if (env.IsDevelopment())
+                    {
+                        await httpContext.Response.WriteAsJsonAsync(new { Errors = "Internal Server Error", exception.StackTrace }, cancellationToken: cancellationToken);
+                    }
+                    else
+                    {
+                        await httpContext.Response.WriteAsJsonAsync(new { Errors = "Internal Server Error" }, cancellationToken: cancellationToken);
+                    }
+
+                    httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                     break;
             }
 
